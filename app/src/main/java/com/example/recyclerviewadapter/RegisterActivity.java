816145d8db6f163232f35a3189291e4d72e14769
HillaryKiprono <kipronohillary240@gmail.com>
2022-07-29 01:07:16 +0300

    initial

--- app/src/main/java/com/example/recyclerviewadapter/RegisterActivity.java
+++ app/src/main/java/com/example/recyclerviewadapter/RegisterActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class RegisterActivity extends AppCompatActivity {
     private Button cancel;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
